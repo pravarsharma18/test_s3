90d67d6e4c356a1ab9a4217f314b5798f7b38d0e
pravar <pravar.sharma@trootech.com>
2023-04-07 18:24:38 +0530

    added create API

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from index import response, get_all_bucket_objects
+from flask import Flask, jsonify, request
+from index import response, get_all_bucket_objects, get_all_bucket_folder, create_s3_bucket
 
 app = Flask(__name__)
 
@@ -10,13 +10,49 @@ def home():
     return jsonify({"buckets": response.get("Buckets")})
 
 @app.route('/<bucket_name>')
-def get_bucket_files(bucket_name):
+def get_bucket_folder(bucket_name):
     try:
         objs = get_all_bucket_objects(bucket_name)
-        data = [{"key": obj['Key'], "last_modified": obj['LastModified'], "size": obj['Size']} for i, obj in enumerate(objs['Contents'])]
+        folder = request.args.get("folder")
+        if folder:
+            objs = get_all_bucket_folder(bucket_name, folder)
+        try:
+            data = [{"key": obj['Prefix']} for i, obj in enumerate(objs['CommonPrefixes'])]
+        except: # noqa
+            data = [{"key": obj['Key'], "last_modified": obj['LastModified'], "size": obj['Size']} for i, obj in enumerate(objs['Contents'])]
         return jsonify({"data": data})
     except Exception as e:
         return jsonify({"error": str(e)})
+
+@app.route('/create', methods=['POST'])
+def create_bucket():
+    try:
+        data = request.get_json()
+        bucket_name = data.get("bucket_name")
+        location = data.get("location")
+        if not bucket_name:
+            return jsonify({"bucket_name": "Bucket name is required."})
+        if not location:
+            return jsonify({"location": "Bucket location is required."})
+        print(data)
+        res = create_s3_bucket(bucket_name, location)
+        print()
+        print(res)
+        return jsonify({"detail": str(res)})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# @app.route('/<bucket_name>/<folder>')
+# def get_bucket_files(bucket_name):
+#     try:
+#         folder = request.GET.get("folder")
+#         print(folder)
+#         objs = get_all_bucket_folder(bucket_name, folder)
+#         print(objs)
+#         # data = [{"key": obj['Prefix']} for i, obj in enumerate(objs['CommonPrefixes'])]
+#         return jsonify({"data": str(objs)})
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 
 if __name__ == "__main__":
--- index.py
+++ index.py
@@ -1,19 +1,36 @@
 import boto3
 import conf
 
+from datetime import date, datetime
+
 s3 = boto3.client('s3', aws_access_key_id=conf.AWS_ACCESS_KEY_ID,
                   aws_secret_access_key=conf.AWS_SECRET_ACCESS_KEY,
                   region_name=conf.S3DIRECT_REGION)
 
 response = s3.list_buckets()
 
-s3.list_objects_v2(Bucket="gready-bucket", Delimiter='/', Prefix="media/")
+# s3.list_objects_v2(Bucket="gready-bucket", Delimiter='/', Prefix="media/")
 
-with open("prefix_delimeter1.txt", "a+") as f:
-    f.write(str(s3.list_objects_v2(Bucket="gready-bucket", Delimiter='/', Prefix="media/Carousel_Images_1/")))
+# with open("prefix_delimeter1.txt", "a+") as f:
+#     f.write(str(s3.list_objects_v2(Bucket="gready-bucket", Delimiter='/')))
 
 def get_all_bucket_objects(bucket_name):
-    return s3.list_objects_v2(Bucket=bucket_name)
+    return s3.list_objects_v2(Bucket=bucket_name, Delimiter='/')
+
+def get_all_bucket_folder(bucket_name, folder):
+    return s3.list_objects_v2(Bucket=bucket_name, Delimiter='/', Prefix=folder)
+
+def create_s3_bucket(bucket_name, location):
+    configuration = {
+        'LocationConstraint': location
+    }
+    res = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=configuration)
+    return res
+
+
+# s3.put_object(Bucket="pravar19921807", Key=("Goa/abc/pqr" + '/'))
+
+# a = s3.delete_objects(Bucket="pravar19921807", Key=("Goa/"))
 
 # bucket = s3.get_bucket("gready-bucket")
 # folders = bucket.list('', '/')
